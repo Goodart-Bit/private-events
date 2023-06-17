47320bf582ae1feed8476db2160b89cc38595055
Goodart-Bit <juandlh416@gmail.com>
2023-06-17 12:42:05 -0500

    Added user model and migration with devise generator

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,28 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_16_183515) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_17_173822) do
   create_table "events", force: :cascade do |t|
     t.datetime "date"
     t.string "name", limit: 255
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.string "first_name", limit: 255
+    t.string "last_name", limit: 255
+    t.date "birthdate"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
