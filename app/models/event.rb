7db327716c84aaf7767f542c48f13ac69baffc12
Goodart-Bit <juandlh416@gmail.com>
2023-06-18 20:46:37 -0500

    Past and upcoming events display on user#show view based on event.rb queries

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,4 +2,6 @@ class Event < ApplicationRecord
   belongs_to :host, class_name: 'User'
   has_many :attendances
   has_many :attendees, through: :attendances
+  scope :past, -> { where 'date < ?', DateTime.now }
+  scope :upcoming, -> { where 'date >= ?', DateTime.now }
 end
